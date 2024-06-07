9960580950fe97b5097b78acf9fa117c40d00d30
Derek Henderson <dshenderson@Dereks-MacBook-Pro.local>
2024-06-07 17:18:35 +0100

    Complete exercise 3

--- src/components/Guess/Guess.tsx
+++ src/components/Guess/Guess.tsx
@@ -1,9 +1,15 @@
 import { range } from "src/utils";
 
-export const Guess = () => {
+interface IGuessProps {
+  guess?: string;
+}
+
+export const Guess = ({ guess = '' }: IGuessProps) => {
+  const letters = guess.split('');
+
   return <div className="guess">
-    {range(5).map(() => (
-      <span className="cell"></span>
+    {range(5).map((_, idx) => (
+      <span className="cell" key={'cell-' + idx}>{ letters[idx] ?? '' }</span>
     ))}
   </div>;
 };
--- src/components/GuessTracker/GuessTracker.tsx
+++ src/components/GuessTracker/GuessTracker.tsx
@@ -1,13 +1,21 @@
+import { NUM_OF_GUESSES_ALLOWED } from 'src/constants';
+import { range } from 'src/utils';
+import { Guess } from '..';
 import { GuessesArray } from '../types';
 
 interface IGuessesProps {
   guesses: GuessesArray;
 }
 
-export const GuessTracker = ({ guesses }: IGuessesProps) => guesses.length ? (
-  <ul className="guess-results">
-    {guesses.map(guess => (
-      <li className="guess" key={guess}>{guess}</li>
-    ))}
-  </ul>
-) : null;
+export const GuessTracker = ({ guesses }: IGuessesProps) => (
+  <div className="guess-results">
+    {range(NUM_OF_GUESSES_ALLOWED).map((_, idx) => {
+      const guess = guesses[idx];
+      const key = guess ?? `guess-${idx}`;
+
+      return (
+        <Guess guess={guess} key={key} />
+      );
+    })}
+  </div>
+);
